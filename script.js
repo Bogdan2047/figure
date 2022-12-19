3eb7ac22811caad16655534d202e0f774af9e140
Bogdan2047 <81803193+Bogdan2047@users.noreply.github.com>
2022-12-19 11:18:34 +0200

    updane

--- script.js
+++ script.js
@@ -36,6 +36,27 @@
 
 //////////////////////////////////////////
 
+// let len = 7;
+
+// let y = 1;
+// let c = len-y/2;
+
+// for(let i = 0; i < len/2; i++){
+
+//    a = '*';
+//    let aa = a.repeat(y);
+
+//     b = " ";
+//     let bb = b.repeat(c);
+
+//     console.log(bb + aa + bb);
+
+//     y = y+2;
+//     c = c-1;
+// }
+
+
+/////////////////////////////////////////
 
 // let len = 7;
 // let midlle = Math.ceil(len/2);
